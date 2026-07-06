485eb644bf7f1fd803fc5dd194932232b943343a
pookie-bear11 <ethangabrieldelapena@gmail.com>
2026-07-06 15:38:15 -0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Ethan Gabriel M. Dela Pena</Text>
+      <Text>Mobile App Development</Text>
+    </View>
+  );
+}
